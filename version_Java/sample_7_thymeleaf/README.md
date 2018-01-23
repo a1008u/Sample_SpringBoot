bff7a72d425c58b4e1c5be0bcc5a4f0e42a2f1e7
Uemoto Akira <a_uemoto@interspace.ne.jp>
2018-01-23 12:33:43 +0900

    エラーメッセージおよびREADME作成

---
+++ version_Java/sample_7_thymeleaf/README.md
@@ -0,0 +1,37 @@
+sample_7_thymeleaf
+====
+
+## Overview
+Spring Bootでthymeleafの使い方を紹介
+
+## Description
+
+    基本はフロントの操作をトリガーにサーバへ送信されて、controllerが動きサーバでの処理を行う。
+    その後、controllerからフロントへ結果を送信する。
+
+> view <-> controller <-> service <-> repository <-> DB(h2)
+
+
+## Demo
+
+## Requirement
+
+## Usage
+
+## Install
+
+ * クーロン
+ $ git clone https://github.com/a1008u/Sample_SpringBoot.git
+
+ * ダウンロード
+ githubよりDLして下さい。
+
+## Contribution
+
+## Licence
+
+
+
+## Author
+
+
--- version_Java/sample_7_thymeleaf/src/main/java/com/example/sample_7_thymeleaf/Form/CustomerForm.java
+++ version_Java/sample_7_thymeleaf/src/main/java/com/example/sample_7_thymeleaf/Form/CustomerForm.java
@@ -1,6 +1,7 @@
 package com.example.sample_7_thymeleaf.Form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -8,9 +9,11 @@ import javax.validation.constraints.Size;
 @Data
 public class CustomerForm {
     @NotNull
-    @Size(min = 1, max = 127)
+    @Size(min = 1, max = 127,message = "{Customer.firstname.fail}")
     private String firstName;
+
     @NotNull
-    @Size(min = 1, max = 127)
+    @Size(min = 1, max = 127, message = "{Customer.lastname.fail}")
     private String lastName;
 }
+
