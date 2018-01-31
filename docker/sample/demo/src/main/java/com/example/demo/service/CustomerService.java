0e8d5219a3ba2490a810cbc521b5e3e95c20aca2
Uemoto Akira <a_uemoto@interspace.ne.jp>
2018-01-31 13:48:21 +0900

    docker(MySQL)とSpringBootの追加

---
+++ docker/sample/demo/src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+
+import com.example.demo.domain.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() { return customerRepository.findAllOrderByName(); }
+
+    public Page<Customer> findAll(Pageable pageable) { return customerRepository.findAllOrderByName(pageable); }
+
+    public Customer findOne(Integer id) {
+        return customerRepository.findOne(id);
+    }
+
+    public Customer create(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer update(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.delete(id);
+    }
+
+    public void deleteAll() {
+        customerRepository.deleteAll();
+    }
+}
